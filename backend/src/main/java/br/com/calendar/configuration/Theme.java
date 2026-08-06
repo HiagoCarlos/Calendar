4c9f1d272fcae52fe3073a2d37d3805d078b14bb
Wendy Konrath <senhordk2@gmail.com>
2026-08-06 04:09:20 -0300

    feat: create default configuration on user signup

---
+++ backend/src/main/java/br/com/calendar/configuration/Theme.java
@@ -0,0 +1,7 @@
+package br.com.calendar.configuration;
+
+public enum Theme {
+    light,
+    dark,
+    system
+}
---
+++ backend/src/main/java/br/com/calendar/configuration/TimeFormat.java
@@ -0,0 +1,6 @@
+package br.com.calendar.configuration;
+
+public enum TimeFormat {
+    h24,
+    h12
+}
